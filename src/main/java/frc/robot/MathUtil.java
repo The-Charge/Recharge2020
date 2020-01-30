1c10e986d397623da5b599f6017fb10ff156691a
Caleb-Team10309 <qiu@hotmail.com>
2020-01-29 20:51:33 -0500

    Finished the generateTrajectory method. The method can take in start and end point as Pose2D objects and waypoint arraylist to generate a trajectory.

--- src/main/java/frc/robot/MathUtil.java
+++ src/main/java/frc/robot/MathUtil.java
@@ -4,8 +4,14 @@ package frc.robot;
 import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 //import org.usfirst.frc2619.Plybot2020.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
 
 public class MathUtil {
 
@@ -66,9 +72,22 @@ public class MathUtil {
 		return (input-min)/(max-min);
 	}
 
-	public void generateTrajectory(Pose2d startPos, Pose2d endPos, ArrayList<Integer> waypoints) {
+	//Need to convert points to Pose2d objects and the way poionts prior to calling this method
+	public Trajectory generateTrajectory(Pose2d startPos, Pose2d endPos, ArrayList<Translation2d> waypoints) {
 
-		
+	/*
+	var sideStart = new Pose2d(Units.feetToMeters(1.54), Units.feetToMeters(23.23), Rotation2d.fromDegrees(-180));
+    var crossScale = new Pose2d(Units.feetToMeters(23.7), Units.feetToMeters(6.8),
+    Rotation2d.fromDegrees(-160));
+
+    var interiorWaypoints = new ArrayList<Translation2d>();
+    interiorWaypoints.add(new Translation2d(Units.feetToMeters(14.54), Units.feetToMeters(23.23)));
+    interiorWaypoints.add(new Translation2d(Units.feetToMeters(21.04), Units.feetToMeters(18.23)));
+	*/
+    TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(12), Units.feetToMeters(12));  //Need to input max velocity and accleration
+    config.setReversed(true);
+
+    return (TrajectoryGenerator.generateTrajectory(startPos, waypoints, endPos, config));
 
 	}
 }
